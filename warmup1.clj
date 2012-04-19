66cc49a2eb880e7af31ddb066f1c85a26a318c3d
Thomas Mesquita <tmesquit@mines.edu>
2012-04-19 10:16:23 -0600

    implemented (collection-type col)

--- warmup1.clj
+++ warmup1.clj
@@ -4,14 +4,26 @@
 ;Aaron Lebahn
 
 ;(big st n) that returns true if a string st is longer than n characters.
-
 (defn big
 	[st n]
-	(> (count st) n))
+	(> (count st) n)
+)
 
-(println (big "test" 4))
-(println (big "Clojure" 3))
-(println (big "Brogrammer" 11))
+(println "st = test, n = 4: " (big "test" 4))
+(println "st = Clojure, n = 3: " (big "Clojure" 3))
+(println "st = Brogrammer, n = 11: " (big "Brogrammer" 11))
 
 ;(collection-type col) that returns :list, :map,
 ;or :vector based on the type of collection col.
+(defn collection-type
+	[col]
+	(cond
+		(list? col) :list
+		(map? col) :map
+		(vector? col) :vector
+	)
+)
+
+(println "'(1 2 3) =" (collection-type '(1 2 3)))
+(println "{:vader \"sith\", :luke \"jedi\"} =" (collection-type {:vader "sith", :luke "jedi"}))
+(println "[:hutt :wookie :ewok] =" (collection-type [:hutt :wookie :ewok]))
