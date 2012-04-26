99968c95bf31448e733f6b346e0eb42ecc3e79f3
Aaron Lebahn <cplusplus328@gmail.com>
2012-04-26 12:31:17 -0600

    Added rudimentry Window Code.

--- project.clj
+++ project.clj
@@ -2,3 +2,18 @@
 ;Thomas Mesquita
 ;Alan Nguyen
 ;Aaron Lebahn
+(import '(javax.swing JFrame JButton JOptionPane))
+
+(defn showWindow "Initializes and shows the main Window" []
+	(let [mainWindow (JFrame. "Tic-Tac-Toe")]
+		(.setDefaultCloseOperation mainWindow JFrame/EXIT_ON_CLOSE)
+		(.setSize mainWindow 300 300)
+		(.setVisible mainWindow true)
+	)
+)
+
+(defn startGame "Starts the Game" []
+	(showWindow)
+)
+
+(startGame)
